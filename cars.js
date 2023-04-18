f4ba28873ad3a6a6bb0369a62bd6a24173cb6901
prabdeep thind <prabdeep_thind@hotmail.com>
2023-04-17 22:38:01 -0400

    first commit

---
+++ cars.js
@@ -0,0 +1,95 @@
+export const cars = [
+    {
+      model: "F8 Tributo",
+      mark: "Ferrari",
+      year: 2021,
+      transmission: "Automatic",
+      horsepower: 710,
+      price: 620,
+      image: "images/FerrariF8Tributo.webp"
+    },
+    {
+      model: "720S Spider",
+      mark: "McLaren",
+      year: 2022,
+      transmission: "Manual",
+      horsepower: 710,
+      price: 620,
+      image: "images/McLaren 720S Spider.webp"
+    },
+    {
+      model: "Aventador",
+      mark: "Lamborghini",
+      year: 2023,
+      transmission: "Automatic",
+      horsepower: 750,
+      price: 680,
+      image: "images/Lamborghini Aventador.png"
+    },
+    {
+      model: "GT3 RS",
+      mark: "Porsche",
+      year: 2023,
+      transmission: "Automatic",
+      horsepower: 518,
+      price: 550,
+      image: "images/Porsche GT3 RS.png"
+    },
+    {
+      model: "GTR",
+      mark: "Nissan",
+      year: 2020,
+      transmission: "Manual",
+      horsepower: 590,
+      price: 585,
+      image: "iimages/Nissan-GT-R-PNG-Transparent.png"
+    },
+    {
+      model: "C8 Z51",
+      mark: "Corvette",
+      year: 2023,
+      transmission: "Automatic",
+      horsepower: 495,
+      price: 520,
+      image: "images/Corvette C8 Z51.png"
+    },
+    {
+      model: "Ghost",
+      mark: "Rolls Royce",
+      year: 2023,
+      transmission: "Automatic",
+      horsepower: 580,
+      price: 570,
+      image: "images/Rolls Royce Ghost.png"
+    },
+    {
+      model: "Vantage",
+      mark: "Aston Martin",
+      year: 2023,
+      transmission: "Automatic",
+      horsepower: 675,
+      price: 600,
+      image: "images/Aston Martin Vantage.png"
+    },
+    {
+      model: "Sport SVR",
+      mark: "Range Rover",
+      year: 2023,
+      transmission: "Automatic",
+      horsepower: 575,
+      price: 560,
+      image: "images/Range Rover Sport SVR.png"
+    },
+    {
+      model: "Continental GT",
+      mark: "Bentley",
+      year: 2023,
+      transmission: "Automatic",
+      horsepower: 610,
+      price: 590,
+      image: "images/Bentley Continental GT.png"
+    }
+  ];
+
+
+
---
+++ index.js
@@ -0,0 +1,59 @@
+import { cars } from "./cars.js";
+let navBarIcon = document.getElementById('navBarIcon');
+let navBar = document.getElementById('nav-bar')
+let closeNavBarIcon = document.getElementById('closeNavBarIcon')
+
+
+navBarIcon.addEventListener('click', function() {
+   navBar.style.display = 'flex';
+   closeNavBarIcon.style.display = 'flex';
+   navBarIcon.style.display = 'none';
+})
+
+closeNavBarIcon.addEventListener('click', function() {
+    navBar.style.display = 'none';
+   closeNavBarIcon.style.display = 'none';
+   navBarIcon.style.display = 'flex';
+})
+
+
+for (let i = 0; i < cars.length; i++) {
+    let carDetails = ` 
+                        <button id="${cars[i].mark}${cars[i].model}">${cars[i].mark}${cars[i].model}</button>
+                        <div id="${cars[i].mark}">
+                            <img src="${cars[i].image}" alt="">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>$${cars[i].price} per day</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>Model</th>
+                                        <td>${cars[i].model}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Mark</th>
+                                        <td>${cars[i].mark}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Year</th>
+                                        <td>${cars[i].year}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Transmission</th>
+                                        <td>${cars[i].transmission}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Horsepower</th>
+                                        <td>${cars[i].horsepower}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                  `;
+    document.getElementById('car-display').innerHTML += carDetails;
+  }
+  
+
